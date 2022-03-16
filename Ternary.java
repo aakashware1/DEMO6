3dd35ca81c3a08fa18964c7c88ee6fa200096091
aakashware1 <101617245+aakashware1@users.noreply.github.com>
2022-03-16 14:46:32 +0530

    Add files via upload

---
+++ Ternary.java
@@ -0,0 +1,21 @@
+package Basic;
+import java.util.*;
+
+
+public class Ternary {
+
+	public static void main(String[] args) {
+		
+		int marks;
+		Scanner s =new Scanner(System.in);
+		System.out.println(" Enter your marks :");
+		marks= s.nextInt();
+		 String result=marks>40 ? "pass":"fail";
+		System.out.println(result);
+				
+				
+		// TODO Auto-generated method stub
+
+	}
+
+}
